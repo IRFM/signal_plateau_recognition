8db63d76b72b31d6cc7deea328f5095b97d90a3b
DUBUS Léo 243615 <leo.dubus@cea.fr>
2024-07-25 11:14:38 +0200

    build: :heavy_plus_sign: add dependencies to setup.py file

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@
 
 from setuptools import setup
 
-setup(name='signal_plateau_recognition',
-      version='0.0.1',
-      description='Signal plateau recognition',
-      author='Jorge Morales',
-      author_email='jorge012@gmail.com',
-      license='MIT',
-      packages=['signal_plateau_recognition'],
-      python_requires='>=3',
-     )
+setup(
+    name="signal_plateau_recognition",
+    version="0.0.1",
+    description="Signal plateau recognition",
+    author="Jorge Morales",
+    author_email="jorge012@gmail.com",
+    license="MIT",
+    packages=["signal_plateau_recognition"],
+    python_requires=">=3",
+    install_requires=[
+        "numpy<2",
+        "scipy",
+        "scikit-learn",
+    ],
+)
